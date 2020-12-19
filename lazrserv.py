cc65eddf07696ad2b5518d8ab9a6b11c2c75f617
Mateco <streitmann.mate99@gmail.com>
2020-12-19 22:20:19 +0100

    Update lazrserv.py

--- lazrserv.py
+++ lazrserv.py
@@ -3,8 +3,8 @@ import select
 
 HEADER_LENGTH = 10
 
-IP = "152.66.179.45"
-PORT = 1234
+IP = '''You set this '''
+PORT = '''Also your choice'''
 
 # Create a socket
 # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
